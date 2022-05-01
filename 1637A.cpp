6a958631d86a46065c4c64b9af5501e3804dd8a4
AvishekPaul <paulavishek222@gmail.com>
2022-05-01 12:34:16 +0600

    cpp

--- 1637A.cpp
+++ 1637A.cpp
@@ -1,36 +1,36 @@
 #include<bits/stdc++.h>
-#include<iostream>
 using namespace std;
 
-int main(){
-    int t,n,count;
-    cin>>t;
-    while(t--){
-        cin>>n;
-        count=0;
-        int a[n],b[n];
-        for(int i=0;i<=n;i++){
-            cin>>a[i];
-            b[i]=a[i];
-        }
-        sort(b, b + n);
+#define int                 long long
+#define vi                  vector<int>
+#define all(c)              (c).begin(), (c).end()
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-        for(int i=0;i<n;i++){
-            cout<<b[i];
-            
-        }
-        for(int i=0;i<n;i++){
-            if(a[i]==b[i]){
-                count++;
-                
-            }
-        }
-        cout<<"count "<<count<<endl;
-        if(count==n){
-            cout<<"NO"<<endl;
-        }
-        else{
-            cout<<"YES"<<endl;
-        }
-    }
+
+void solution() {
+	int n;
+	cin >> n;
+
+	vi arr(n);
+
+	FOR (i, 0, n) {
+		cin >> arr[i];
+	}
+
+	cout << (is_sorted(all(arr)) ? "NO" : "YES");
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		solution();
+		cout << '\n';
+	}
+
+	return 0;
 }
